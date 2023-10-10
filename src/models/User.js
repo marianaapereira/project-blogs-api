20024d7171c14f48439a48ff9e0c8051a64e580e
Mariana Pereira <marianapereira.s@live.com>
2023-10-10 10:55:32 -0300

    Criação do model User

---
+++ src/models/User.js
@@ -0,0 +1,15 @@
+const { DataTypes } = require("sequelize");
+
+const UserModel = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: DataTypes.INTEGER,
+    display_name: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+  });
+
+  return User;
+}
+
+module.exports = UserModel;
